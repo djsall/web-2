56ff417c97fcbfec19e74a97313a041feb9a423a
Levente Proksa <proksalevente@gmail.com>
2023-11-30 18:00:23 +0100

    pastry homepage

--- README.md
+++ README.md
@@ -12,6 +12,27 @@
 - clone this repository
 - install the packages. ```composer install```, ```npm install```
 - import the database
-- copy the ```.env.example``` file into the ```.env``` file and set up your database name (```DB_DATABASE=```) and credentials
+- copy the ```.env.example``` file into the ```.env``` file and set up your database name (```DB_DATABASE=```) and
+  credentials
 - run the ```php artisan key:generate``` command to make the cryptography key for the app
 - build the assets with the ```npm run build``` terminal command
+- create an administrative user ```php artisan make:filament-user```. After creating, you will have to change the
+  user's ```role``` in the database from ```user``` to ```admin```.
+- you can also use the applications web interface to sign up, it's the same result and you still will have to change the
+  type of the first user manually. After setting up the first user manually, you can
+  visit ```<web_address_of_app>/admin``` to add new users or manage existing ones.
+
+### Admin panel features
+
+Access the admin panel at ```<web_address_of_app>/admin```
+
+- you can manage the users of the application (2 / d)
+- you can upload navigation items, which are stored inside the database. Please use full URL's, route parsing from
+  databas links is not supported (2 / b)
+
+### RESTFUL API
+- use the ```Restful postman.json``` file provided in the repository, to test the ```pastry``` API resource. (4)
+
+### Application
+- visiting the homepage will reveal the company introduction page (1)
+- clicking the Laravel logo on the navigation bar will take you to the login page
--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -1,36 +1,37 @@
 <x-app-layout>
-    <div class="grid md:grid-cols-2">
+    <section class="grid md:grid-cols-2 md:mt-64 md:scroll-mt-64" id="hero">
         <div class="flex items-center justify-center">
-            <h1 class="text-center text-4xl md:text-8xl font-bold py-12 md:py-16">
-                Welcome to our dog barber shop!
+            <h1 class="text-center text-4xl md:text-[200px] md:leading-[200px] font-bold py-12">
+                Welcome to our <span class="text-blue-700 underline hover:text-blue-950">pastry</span> shop!
             </h1>
         </div>
-        <img src="https://www.shutterstock.com/image-photo/happy-puppy-welsh-corgi-14-600nw-2270841247.jpg"
-             alt="Happy Corgi" class="w-full h-full max-h-[1200px] object-contain">
-    </div>
-    <div class="mt-32">
-        <h2 class="text-center text-3xl md:text-6xl font-bold mb-16">
-            Your dog is really safe with us.
+        <img
+            src="https://hips.hearstapps.com/hmg-prod/images/man-dispensing-cakes-in-a-pastry-bakery-royalty-free-image-1676319170.jpg?crop=0.88889xw:1xh;center,top&resize=1200:*"
+            alt="Happy pastries" class="w-full h-full max-h-[1400px] object-contain aspect-square md:p-12">
+    </section>
+    <section class="mt-32 md:mt-[800px] md:scroll-mt-[700px]" id="intro">
+        <h2 class="text-center text-3xl md:text-8xl font-bold mb-16">
+            Your pastry needs are our <u class="hover:text-blue-950 text-blue-700">mission.</u>
         </h2>
-        <p class="text-xl md:text-2xl p-5 md:text-center">
-            Our passion is dogs.<br>
-            We bathe, groom, trim and cut them where they require it,<br>
-            with utmost care to their mental and physical well-being.
+        <p class="text-xl md:text-4xl p-5 md:text-center">
+            Our passion is pastry.<br>
+            We bake, make, knead and dress pastries for a living,<br>
+            with utmost care to their taste and appearance.
         </p>
-    </div>
-    <div class="flex justify-center mt-16 pb-32">
-        <a href="{{ "" }}"
-        class="px-12 py-8 text-white bg-blue-700 font-bold text-lg hover:bg-blue-950 hover:text-opacity-70 rounded-lg">
-            Grab an appointment
-        </a>
-    </div>
-    <div>
+        <div class="flex justify-center mt-32 pb-32">
+            <a href="{{ "" }}"
+               class="px-12 py-8 text-white bg-blue-700 font-bold text-lg hover:bg-blue-950 hover:text-opacity-70 rounded-lg">
+                View our products
+            </a>
+        </div>
+    </section>
+    <section class="md:mt-96 md:pt-[500px] md:pb-[125px]" id="about-us">
         <p class="text-lg p-5 md:text-xl md:text-center text-gray-600">
-            We have been in the dog grooming business for decades.<br>
-            My mother, <span class="text-blue-700">Alice</span>, has been a dog groomer since late thirties, and<br>
+            We have been in the pastry business for decades.<br>
+            My mother, <span class="text-blue-700">Alice</span>, has been a baker since her late thirties, and<br>
             I've taken it upon myself to provide her service to our loyal customers.<br>
             We are a small, family owned business,<br>
-            which has been serving our community since <u class="text-black">1981</u>.
+            which has been serving our community since <u class="text-black">1991</u>.
         </p>
-    </div>
+    </section>
 </x-app-layout>
